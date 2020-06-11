4adee212ab9bbcb882790aa49b6cf16884c43151
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2020-06-11 16:07:18 +0800

    fixed custom scroll duration invalid, remove ViewPager onTouchEvent listener

--- exbanner/src/main/java/com/weicools/banner/ExViewPager.java
+++ exbanner/src/main/java/com/weicools/banner/ExViewPager.java
@@ -1,6 +1,5 @@
 package com.weicools.banner;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,14 +15,8 @@ import java.lang.reflect.Field;
  * @date 2020.06.10
  */
 class ExViewPager extends ViewPager {
-  public interface ActionListener {
-    void handleDown();
-
-    void handleLeave();
-  }
-
   private static class ExScroller extends Scroller {
-    private int duration = 1000;
+    private int scrollDuration = 1000;
 
     public ExScroller(Context context) {
       super(context);
@@ -39,24 +32,21 @@ class ExViewPager extends ViewPager {
 
     @Override
     public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-      super.startScroll(startX, startY, dx, dy, duration);
+      super.startScroll(startX, startY, dx, dy, scrollDuration);
     }
 
     @Override
     public void startScroll(int startX, int startY, int dx, int dy) {
-      super.startScroll(startX, startY, dx, dy, duration);
+      super.startScroll(startX, startY, dx, dy, scrollDuration);
     }
 
     public void setDuration(int duration) {
-      this.duration = duration;
+      this.scrollDuration = duration;
     }
   }
 
   @Nullable
   private ExScroller exScroller;
-
-  @Nullable
-  private ActionListener actionListener;
 
   private boolean disableTouchScroll;
 
@@ -79,18 +69,14 @@ class ExViewPager extends ViewPager {
         return;
       }
 
-      exScroller = new ExScroller(getContext());
-      exScroller.setDuration(duration);
       Field mField = ViewPager.class.getDeclaredField("mScroller");
       mField.setAccessible(true);
+      exScroller = new ExScroller(getContext());
+      exScroller.setDuration(duration);
       mField.set(this, exScroller);
     } catch (Exception e) {
       e.printStackTrace();
     }
-  }
-
-  public void setActionListener(@Nullable ActionListener actionListener) {
-    this.actionListener = actionListener;
   }
 
   @Override
@@ -103,31 +89,5 @@ class ExViewPager extends ViewPager {
     } catch (Exception ignore) {
       return false;
     }
-  }
-
-  @Override
-  @SuppressLint("ClickableViewAccessibility")
-  public boolean onTouchEvent(MotionEvent ev) {
-    if (disableTouchScroll) {
-      return false;
-    }
-
-    switch (ev.getAction()) {
-      case MotionEvent.ACTION_DOWN:
-        if (actionListener != null) {
-          actionListener.handleDown();
-        }
-        break;
-      case MotionEvent.ACTION_UP:
-      case MotionEvent.ACTION_CANCEL:
-      case MotionEvent.ACTION_OUTSIDE:
-        if (actionListener != null) {
-          actionListener.handleLeave();
-        }
-        break;
-      default:
-        break;
-    }
-    return super.onTouchEvent(ev);
   }
 }
